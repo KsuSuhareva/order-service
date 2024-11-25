38b96190a56f463f30322fd8283d9d154ffdc6d9
ksuhareva <ksuhareva@intervale.ru>
2024-11-25 14:13:58 +0300

    не большие правки

--- src/main/java/com/suhareva/order_service/config/WebclientConfiguration.java
+++ src/main/java/com/suhareva/order_service/config/WebclientConfiguration.java
@@ -14,6 +14,7 @@ public class WebclientConfiguration {
 
     @Bean
     public WebClient getWebClient(@Value("${generateNumberService.url}") String url) {
+        log.info("Set Webclient baseUrl " + url);
         return WebClient.builder()
                 .baseUrl(url)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
--- src/main/java/com/suhareva/order_service/service/generateNumber/impl/NumberGenerateServiceImpl.java
+++ src/main/java/com/suhareva/order_service/service/generateNumber/impl/NumberGenerateServiceImpl.java
@@ -3,6 +3,7 @@ package com.suhareva.order_service.service.generateNumber.impl;
 import com.suhareva.order_service.dto.OrderNumber;
 import com.suhareva.order_service.service.generateNumber.NumberGenerateService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.util.retry.Retry;
@@ -11,11 +12,13 @@ import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NumberGenerateServiceImpl implements NumberGenerateService {
     private final WebClient webClient;
 
     @Override
     public OrderNumber generate() {
+        log.info("Start Webclient post");
         return webClient
                 .post()
                 .uri("/numbers/generateNumber")
